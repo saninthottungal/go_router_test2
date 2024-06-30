15fbeaa88fe62259131bfe5fa95032d5c26a4330
sanin <sanin@pietechsolution.com>
2024-06-30 20:49:03 +0530

    add function is working

--- lib/pages/new_note/new_note_page.dart
+++ lib/pages/new_note/new_note_page.dart
@@ -1,28 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:go_route_test2/pages/home/home_page.logic.dart';
+import 'package:go_route_test2/pages/home/home_page.state.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class NewNotePage extends StatelessWidget {
+class NewNotePage extends HookConsumerWidget {
   const NewNotePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final titleController = useTextEditingController();
+    final descController = useTextEditingController();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add a new note!'),
       ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
         child: Column(
           children: [
             TextField(
-              decoration: InputDecoration(
+              controller: titleController,
+              decoration: const InputDecoration(
                 hintText: "Title...",
                 border: OutlineInputBorder(),
               ),
             ),
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
             TextField(
+              controller: descController,
               maxLines: 10,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 hintText: "Note...",
                 border: OutlineInputBorder(),
               ),
@@ -31,7 +39,16 @@ class NewNotePage extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {},
+        onPressed: () {
+          final title = titleController.text;
+          final desc = descController.text;
+          if (desc.isNotEmpty) {
+            ref
+                .read(notesProvider.notifier)
+                .addNote(Note.create(title: title, description: desc));
+          }
+          Navigator.pop(context);
+        },
         label: const Text("Add Note"),
       ),
     );
