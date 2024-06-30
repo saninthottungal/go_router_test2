f82718e6827db82a0edc1ebb04c6af42f1384993
sanin <sanin@pietechsolution.com>
2024-06-30 21:19:55 +0530

    base routes added

---
+++ lib/routes/routes.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:go_route_test2/pages/home/home_page.dart';
+import 'package:go_route_test2/pages/new_note/new_note_page.dart';
+import 'package:go_router/go_router.dart';
+
+@TypedGoRoute<HomePageRoute>(path: '/')
+class HomePageRoute extends GoRouteData {
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return const HomePage();
+  }
+}
+
+@TypedGoRoute<NewNoteRoute>(path: '/new')
+class NewNoteRoute extends GoRouteData {
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return const NewNotePage();
+  }
+}
