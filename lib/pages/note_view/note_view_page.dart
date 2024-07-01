e1314aade4676a5bcc346363b13cf1676e1045f6
sanin <sanin@pietechsolution.com>
2024-07-01 11:15:49 +0530

    route for new page added

--- lib/pages/note_view/note_view_page.dart
+++ lib/pages/note_view/note_view_page.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:go_route_test2/pages/home/home_page.state.dart';
 
 class NoteViewPage extends StatelessWidget {
-  const NoteViewPage({super.key});
+  const NoteViewPage({super.key, required this.note});
+
+  final Note note;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("title"),
+        title: Text(note.title),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(10),
-        child: Text("data"),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Text(note.description),
       ),
     );
   }
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_route_test2/pages/home/home_page.dart';
+import 'package:go_route_test2/pages/home/home_page.state.dart';
 import 'package:go_route_test2/pages/new_note/new_note_page.dart';
+import 'package:go_route_test2/pages/note_view/note_view_page.dart';
 import 'package:go_router/go_router.dart';
 part 'routes.g.dart';
 
@@ -9,6 +11,16 @@ class HomePageRoute extends GoRouteData {
   @override
   Widget build(BuildContext context, GoRouterState state) {
     return const HomePage();
+  }
+}
+
+@TypedGoRoute<NoteViewRoute>(path: '/note_view')
+class NoteViewRoute extends GoRouteData {
+  NoteViewRoute({required this.$extra});
+  final Note $extra;
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return NoteViewPage(note: $extra);
   }
 }
 
